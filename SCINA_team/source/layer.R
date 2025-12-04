b9d6a26243caf105910746c342cf933b56bb0a17
sir-w7 <vohwinston@gmail.com>
2025-12-03 20:09:41 -0800

    Reorganized some code and added SCINA team specific .gitignore

--- SCINA_team/source/layer.R
+++ SCINA_team/source/layer.R
@@ -15,17 +15,4 @@ import <- function(package)
 	library(package, character.only=TRUE)
 }
 
-download_dataset <- function(url, dir)
-{
-	# Will change to proper download directory later
-	# (for testing purposes only)
-	download_dest <- paste(dir, basename(url), sep="/")
-	if (!file.exists(download_dest)) {
-		print(paste("Downloading dataset", basename(url)))
-		download.file(ssread_data_url, download_dest, "curl", timeout=600)
-	} else {
-		# print(paste("Dataset", basename(url), "already downloaded"))
-	}
 
-	return(download_dest)
-}
--- SCINA_team/source/main.R
+++ SCINA_team/source/main.R
@@ -10,8 +10,14 @@
 
 # This here thing is a little weird to use. 
 # TODO(winston): Implement a better method than this
-library(here)
+# not very generic because here must first be installed
+
+library(here) 
 source(here("source", "layer.R"))
+
+
+################################################################################
+# NOTE(winston): ~packages~
 
 import("BiocManager")
 import("SCINA")
@@ -19,6 +25,87 @@ import("qs")
 
 import("Seurat")
 import("ggplot2")
+
+
+
+
+################################################################################
+# NOTE(winston): ~global variables~
+# Marker list
+AD_markers <- list(
+	Astrocyte = c("GFAP", "AQP4", "GJA1", "SLC1A2", "FGFR3", "NKAIN4", 
+		"AGT", "PLXNB1", "SLC1A3"),
+	Microglia  = c("P2RY12", "CSF1R", "C3", "CX3CR1"),
+	Oligodendrocyte = c("OLIG2", "MBP", "MOBP", "PLP1", "MYRF", "MAG"),
+	OPC = c("VCAN", "SOX8"),
+	ExcitatoryNeuron = c("SLC17A6", "SLC17A7", "SATB2"),
+	InhibitoryNeuron = c("GAD1", "GAD2"),
+	Endothelial = c("CLDN5", "VWF"),
+	Pericyte = c("AMBP", "HIGD1B", "PTH1R"),
+	Neuron = c("GLS", "RBFOX3", "CAMK2A"),
+	OurMarkers = c("APOE", "APP", "ACE", "GRN", "APH1B")
+)
+
+
+
+################################################################################
+# NOTE(winston): ~functions~
+download_dataset <- function(url, dir)
+{
+	# Will change to proper download directory later
+	# (for testing purposes only)
+	download_dest <- paste(dir, basename(url), sep="/")
+	if (!file.exists(download_dest)) {
+		print(paste("Downloading dataset", basename(url)))
+		download.file(ssread_data_url, download_dest, "curl", timeout=600)
+	} else {
+		cat("Dataset", basename(url), "already downloaded\n")
+	}
+
+	return(download_dest)
+}
+
+generate_barplot <- function(scina_res, output_file)
+{
+	#make SCINA output into a clean dataframe
+	cell_labels <- scina_res$cell_labels
+	prob_mat <- scina_res$probabilities
+
+	row_index <- match(cell_labels, rownames(prob_mat))
+	assigned_probs <- rep(NA, length(cell_labels))
+	valid <- which(!is.na(row_index))
+
+	assigned_probs[valid] <- prob_mat[cbind(row_index[valid], valid)]
+
+	result_df <- data.frame(
+		cell = colnames(prob_mat),
+		label = cell_labels,
+		probability = assigned_probs
+	)
+
+	cat("Generating bar plot", output_file, "\n")
+	bar_plot <- ggplot(data = result_df, 
+		aes(x = cell_labels)) + geom_bar() + 
+		theme(axis.text.x = element_text(angle = 45, hjust = 1))
+	ggsave(output_file)
+}
+
+generate_heatplot <- function(scina_res, output_file)
+{
+	pdf(output_file)
+	cat("Generating heatmap", output_file, "\n")
+	plotheat.SCINA(gene_exp_mat, result, AD_markers)
+	cat("Heatmap generated\n")
+	dev.off()
+}
+
+
+
+
+
+
+################################################################################
+# NOTE(winston): ~main code~
 
 # ssread_data_url <- "https://bmblx.bmi.osumc.edu/ssread_download/scrnaseq_qsave/AD03501.qsave"
 
@@ -29,20 +116,6 @@ dest <- download_dataset(ssread_data_url, here("ext"))
 dataset <- qs::qread(dest)
 
 
-# Marker list
-AD_markers <- list(
-   Astrocyte = c("GFAP", "AQP4", "GJA1", "SLC1A2", "FGFR3", "NKAIN4", "AGT", "PLXNB1", "SLC1A3"),
-   Microglia  = c("P2RY12", "CSF1R", "C3", "CX3CR1"),
-   Oligodendrocyte = c("OLIG2", "MBP", "MOBP", "PLP1", "MYRF", "MAG"),
-   OPC = c("VCAN", "SOX8"),
-   ExcitatoryNeuron = c("SLC17A6", "SLC17A7", "SATB2"),
-   InhibitoryNeuron = c("GAD1", "GAD2"),
-   Endothelial = c("CLDN5", "VWF"),
-   Pericyte = c("AMBP", "HIGD1B", "PTH1R"),
-   Neuron = c("GLS", "RBFOX3", "CAMK2A"),
-   OurMarkers = c("APOE", "APP", "ACE", "GRN", "APH1B")
-)
-
 # So, for reference, layers are known as:
 # count: unnormalized
 # data: normalized
@@ -51,35 +124,24 @@ gene_exp_norm <- GetAssayData(object=dataset, assay="RNA", layer="data")
 gene_exp_mat <- as.matrix(gene_exp_norm)
 
 # Using SCINA settings from the SCINA github
-result <- SCINA(gene_exp_mat, AD_markers,
-				max_iter = 100, convergence_n = 10,
-				convergence_rate = 0.999, sensitivity_cutoff = 0.9,
-				rm_overlap=TRUE, allow_unknown=FALSE, log_file='SCINA.log')
+# Profiling using system.time instead of Rprof (may change)
 
-#View(result$cell_labels)
-#View(result$probabilities)
+cat("Running SCINA...\n")
+SCINA_prof <- system.time({
+	result <- SCINA(gene_exp_mat, AD_markers,
+		max_iter = 100, convergence_n = 10,
+		convergence_rate = 0.999, sensitivity_cutoff = 0.9,
+		rm_overlap=TRUE, allow_unknown=FALSE, log_file="SCINA.log")
+})
 
-# theoretically, this would write heatmap to pdf
-#pdf(here("heatmap.pdf"), width=10, height=8)
-plotheat.SCINA(gene_exp_mat, result, AD_markers)
-#dev.off()
+cat("SCINA complete!\n")
 
-#make SCINA output into a clean dataframe
-cell_labels <- result$cell_labels
-prob_mat <- result$probabilities
+heatplot_prof <- system.time(generate_heatplot(result, here("output", "AD00203_heatplot.png")))
+barplot_prof <- system.time(generate_barplot(result, here("output", "AD00203_barplot.png")))
 
-row_index <- match(cell_labels, rownames(prob_mat))
-assigned_probs <- rep(NA, length(cell_labels))
-valid <- which(!is.na(row_index))
-
-assigned_probs[valid] <- prob_mat[cbind(row_index[valid], valid)]
-
-result_df <- data.frame(
-  cell = colnames(prob_mat),
-  label = cell_labels,
-  probability = assigned_probs
-)
-
-#bar plot below
-bar_plot <- ggplot(data = result_df, aes(x = cell_labels)) + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-bar_plot
+cat("SCINA time\n")
+print(SCINA_prof)
+cat("heatplot time\n")
+print(heatplot_prof)
+cat("barplot time\n")
+print(barplot_prof)
