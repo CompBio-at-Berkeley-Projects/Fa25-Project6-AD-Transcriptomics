e866446af7b6a306f005c9dabe9ec61393bf2779
sir-w7 <vohwinston@gmail.com>
2025-12-03 23:55:32 -0800

    Added tentative solution for SCINA unknown cell types based on implementation explained in ssREAD paper. Will need to further tweak SCINA settings.

--- SCINA_team/source/main.R
+++ SCINA_team/source/main.R
@@ -20,6 +20,8 @@ import("qs")
 
 import("Seurat")
 
+import("dplyr")
+
 import("ggplot2")
 import("cowplot")
 
@@ -29,19 +31,20 @@ import("cowplot")
 # NOTE(winston): ~global variables~
 # Marker list
 AD_markers <- list(
+	Oligodendrocyte = c("OLIG2", "MBP", "MOBP", "PLP1", "MYRF", "MAG"),
 	Astrocyte = c("GFAP", "AQP4", "GJA1", "SLC1A2", "FGFR3", "NKAIN4", 
 		"AGT", "PLXNB1", "SLC1A3"),
-	Microglia  = c("P2RY12", "CSF1R", "C3", "CX3CR1"),
-	Oligodendrocyte = c("OLIG2", "MBP", "MOBP", "PLP1", "MYRF", "MAG"),
 	OPC = c("VCAN", "SOX8"),
+	Endothelial = c("CLDN5", "VWF"),
+	Microglia  = c("P2RY12", "CSF1R", "C3", "CX3CR1"),
 	ExcitatoryNeuron = c("SLC17A6", "SLC17A7", "SATB2"),
 	InhibitoryNeuron = c("GAD1", "GAD2"),
-	Endothelial = c("CLDN5", "VWF"),
-	Pericyte = c("AMBP", "HIGD1B", "PTH1R"),
-	Neuron = c("GLS", "RBFOX3", "CAMK2A"),
-	OurMarkers = c("APOE", "APP", "ACE", "GRN", "APH1B")
+	Pericyte = c("AMBP", "HIGD1B", "PTH1R")
+	#Neuron = c("GLS", "RBFOX3", "CAMK2A")
+#	OurMarkers = c("APOE", "APP", "ACE", "GRN", "APH1B")
 )
 
+our_markers <- c("APOE", "APP", "ACE", "GRN", "APH1B")
 
 
 ################################################################################
@@ -68,7 +71,7 @@ generate_barplot <- function(scina_res, output_file)
 	prob_mat <- scina_res$probabilities
 
 	row_index <- match(cell_labels, rownames(prob_mat))
-	ssigned_probs <- rep(NA, length(cell_labels))
+	assigned_probs <- rep(NA, length(cell_labels))
 	#valid <- which(!is.na(row_index))
 
 	valid <- 1:length(cell_labels)
@@ -124,15 +127,38 @@ gene_exp_mat <- as.matrix(gene_exp_norm)
 # Using SCINA settings from the SCINA github
 # Profiling using system.time instead of Rprof (may change)
 
+
+# This SCINA stuff and assignment for the unknowns still need some tweaking
 cat("Running SCINA...\n")
 SCINA_prof <- system.time({
 	result <- SCINA(gene_exp_mat, AD_markers, 
 		max_iter = 100, convergence_n = 10, convergence_rate = 0.999, 
-		sensitivity_cutoff = 0.9, rm_overlap=TRUE, allow_unknown=FALSE, 
+		sensitivity_cutoff = 0.9, rm_overlap=TRUE, allow_unknown=TRUE, 
 		log_file="SCINA.log")
+	# result <- SCINA(gene_exp_mat, AD_markers, allow_unknown=FALSE,
+		#log_file="SCINA.log")
 })
 
 cat("SCINA complete!\n")
+
+
+dataset$scina_labels <- result$cell_labels
+names(dataset$scina_labels) <- colnames(dataset)
+
+cluster_majority <- dataset@meta.data %>%
+	group_by(seurat_clusters) %>%
+	filter(scina_labels != "unknown") %>%
+	summarize(major_label=names(which.max(table(scina_labels))))
+better_labels <- dataset$scina_labels
+
+for (cluster in unique(dataset$seurat_clusters)) {
+	majority <- cluster_majority$major_label[cluster_majority$seurat_clusters == cluster]
+	unknown <- WhichCells(dataset, 
+		expression=(scina_labels=="unknown" & seurat_clusters == cluster))
+	better_labels[unknown] <- majority
+}
+
+dataset$scina_but_better_labels <- better_labels
 
 # heatplot_prof <- system.time(generate_heatplot(result, "output/AD00203_heatplot.png"))
 barplot_prof <- system.time(generate_barplot(result, "output/AD00203_barplot.png"))
@@ -152,7 +178,9 @@ sink("output/result_prob.txt")
 result$probabilities
 sink()
 
-dataset$scina_labels <- result$cell_labels
+
+#levels(Idents(dataset)) <- c("Astrocytes", "Endothelial cells", "Excitatory neurons", 
+#	"Microglia", "Oligodendrocytes", "Oligodendrocyte precursor cells")
 
 DimPlot(dataset, reduction="umap")
 ggsave("output/seurat_dimplot.png")
@@ -160,6 +188,12 @@ ggsave("output/seurat_dimplot.png")
 DimPlot(dataset, reduction="umap", group.by="scina_labels")
 ggsave("output/scina_dimplot.png")
 
-FeaturePlot(dataset, reduction="umap", features=AD_markers$OurMarkers) 
+DimPlot(dataset, reduction="umap", group.by="scina_but_better_labels")
+ggsave("output/better_scina_dimplot.png")
+
+
+# TODO(winston): fix it so it uses SCINA labelling instead of fricking 
+# Seurat labelling
+FeaturePlot(dataset, reduction="umap", features=our_markers) 
 ggsave("output/seurat_featureplot.png")
 
